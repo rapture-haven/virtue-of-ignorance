f0414c92a408ac9f31777649a386769dfa65771e
Maxim <mad-maxkozlov@mail.ru>
2022-04-24 16:26:44 +0300

    #22 update store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,11 +1,25 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { get, set } from '~/utils/localstorage'
+import { useAccessor, mutationTree, actionTree } from 'typed-vuex'
 
-Vue.use(Vuex);
+export const state = () => ({
+  displayname: get<string>('displayname', ''),
+  password: get<string>('password', ''),
+  active: false,
+  connecting: false,
+  connected: false,
+  locked: {} as Record<string, boolean>,
+})
 
-export default new Vuex.Store({
-  state: {},
+export const storePattern = {
+  state: state,
   mutations: {},
   actions: {},
   modules: {},
-});
+}
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store(storePattern)
+export const accessor = useAccessor(store, storePattern)
