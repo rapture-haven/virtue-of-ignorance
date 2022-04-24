cbad15bcc28324ba1011e8d4912dc0db591a4d51
Maxim <mad-maxkozlov@mail.ru>
2022-04-24 19:35:09 +0300

    #21 init main page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div id="virtue">
+    <virtue-connect v-if="!connected" />
+    <notifications
+      group="virtue"
+      position="top left"
+      style="top: 50px; pointer-events: none"
+      :ignoreDuplicates="true"
+    />
   </div>
 </template>
 
