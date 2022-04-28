cb8a0e2cfdfd9832eb46971e27576f6ab47645c8
mr-okto <maxim.myshonkov@gmail.com>
2022-04-28 21:43:31 +0300

    Feature/41 http (#42)
    * #41 Add http server
    
    * #41 Add http config

--- server/cmd/app/main.go
+++ server/cmd/app/main.go
@@ -1,12 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
 
-// Hello just says hello
-func Hello() {
-	fmt.Println("hello, world!")
-}
+	"rapture-haven/virtue-of-ignorance"
+	"rapture-haven/virtue-of-ignorance/internal/types/config"
+)
 
-func main() {
-	Hello()
+// функция инициализации сервера через cobra
+func init() {
+	command := &cobra.Command{
+		Use:   "serve",
+		Short: "serve virtue streaming server",
+		Long:  `serve virtue streaming server`,
+		Run:   virtue.Service.ServeCommand,
+	}
+
+	configs := []config.Config{
+		virtue.Service.Server,
+		virtue.Service.WebRTC,
+		virtue.Service.Remote,
+		virtue.Service.Broadcast,
+		virtue.Service.WebSocket,
+	}
+
+	cobra.OnInitialize(func() {
+		for _, cfg := range configs {
+			cfg.Set()
+		}
+		virtue.Service.Preflight()
+	})
+
+	for _, cfg := range configs {
+		if err := cfg.Init(command); err != nil {
+			log.Panic().Err(err).Msg("unable to run serve command")
+		}
+	}
+
+	root.AddCommand(command)
 }
